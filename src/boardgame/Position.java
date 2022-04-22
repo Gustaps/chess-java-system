bfe8c14a1d06bcaa4470aed2daea866a433f4098
Gustaps <gustaps@gmail.com>
2022-04-22 16:13:15 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,33 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	public Position(int line, int column) {
+		this.row = line;
+		this.column = column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getColumn() {
+		return column;
+	}
+
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return row + ", " + column;
+	}
+}
